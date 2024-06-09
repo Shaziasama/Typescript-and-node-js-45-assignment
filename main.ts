0ce997b6dcbb74f725d5d6ef2aef7bd069aa35a1
Shaziasama <shaziasamma921@gmail.com>
2024-06-09 17:10:55 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,31 +1,7 @@
-import { reverse } from "dns";
-import { textSpanContainsPosition } from "typescript";
 
-//store the location in arry alphabetical order
-let countriesToVisit: string[] = ["China","Denmark","Bareen","  Australia",];
-//ptint the arry original odrder
-console.log("original oder: " , countriesToVisit);
+ let guestList: string[] =["hina","arsalan","ubaid" ,"shazia"];
 
-//print the arry in alphanbitical oder without modifying the actual l
-console.log("alphabetical order: " ,[...countriesToVisit].sort());
 
-//show that the arry is still in its original order
-console.log("still aphabetical order:", countriesToVisit);
 
-//print the arry in reverse order
-console.log("Reverse order:" ,[...countriesToVisit].reverse());
-
-//show that the arry is still in orginal oder
-console.log("still aphabetical order:", countriesToVisit);
-
-// we have changed the original arry oder now
-console.log("original Arry reverse:", countriesToVisit.reverse());
-
-//print the arry to show its original oder
-console.log("back to original arry:", countriesToVisit.reverse());   
-
-//print the arry to show and change alphabetical oder
-console.log("sorted in alphabetical order:", countriesToVisit.sort());
-
-//arry reverse again
-console.log("original arry Reversed:", countriesToVisit.reverse());
+let lengthGuest: number = guestList.length;
+console.log(`we are invitng total ${lengthGuest} guests.`);
