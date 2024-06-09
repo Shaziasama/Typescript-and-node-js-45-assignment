6d8408740c4e65cce7dc4067e950a22a1edfb3e7
Shaziasama <shaziasamma921@gmail.com>
2024-06-09 19:41:14 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 var alien_color = "green";
-if (alien_color === "green") {
-    console.log("plyers just earn 5 point for shooting the alien");
+if (alien_color == "green") {
+    console.log("you shot down green alien you have earned 5 points");
 }
-else {
-    console.log("plyers just eran 5 points");
+else if (alien_color === "yellow") {
+    console.log("you shot down yellow alien you have earned 10 points");
 }
-if (alien_color === "blue") {
-    console.log("my color is blue");
+else if (alien_color === "red") {
+    console.log("you shot down red alien you have earned 15 points");
 }
-else {
-    (alien_color === "blue");
-    console.log(" i m come from else statement");
+//second version 
+var alien_color2 = "yellow";
+if (alien_color2 === "Green") {
+    console.log("you shot down yellow alien you have earned 5 points");
+}
+else if (alien_color2 === "yellow") {
+    console.log("you shot down yellow alien you have earned 10 points");
+}
+else if (alien_color2 === "red") {
+    console.log("you shot down red alien you have earned 15 points");
+}
+//version 3
+var alien_color3 = "red";
+if (alien_color3 === "green") {
+    console.log("you shot down green alien you have earned 5 points");
+}
+else if (alien_color3 === "yellow") {
+    console.log("you shot down yellow alien you have earned 10 points");
+}
+else if (alien_color3 === "red") {
+    console.log("you shot down red alien you have earned 15 points");
 }
--- main.ts
+++ main.ts
@@ -1,15 +1,34 @@
 let alien_color = "green";
 
-if (alien_color === "green") {
-    console.log("plyers just earn 5 point for shooting the alien");
-}    
-else {
-console.log("plyers just eran 5 points");
+if (alien_color == "green") {
+    console.log("you shot down green alien you have earned 5 points");
 }
- if (alien_color === "blue") {
-    console.log(" i m come from if statement");
+else if(alien_color === "yellow"){
+    console.log("you shot down yellow alien you have earned 10 points");
+}
+else if (alien_color === "red"){
+    console.log("you shot down red alien you have earned 15 points");
+}
+
+//second version 
+let alien_color2 = "yellow" 
+if (alien_color2 === "Green"){
+    console.log("you shot down yellow alien you have earned 5 points");
+}
+else if (alien_color2 === "yellow"){
+    console.log("you shot down yellow alien you have earned 10 points");
+}
+else if(alien_color2 === "red"){
+    console.log("you shot down red alien you have earned 15 points");
+ } 
+ //version 3
+ let alien_color3 = "red";
+ if (alien_color3 === "green"){
+     console.log("you shot down green alien you have earned 5 points");
  }
- else{ (alien_color === "blue")
-    console.log(" i m come from else statement");
- }
- 
+else if (alien_color3 === "yellow"){
+    console.log("you shot down yellow alien you have earned 10 points");
+}
+else if (alien_color3 === "red"){
+    console.log("you shot down red alien you have earned 15 points");
+}
