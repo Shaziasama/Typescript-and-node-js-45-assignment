a431e8344167c8973a4b27c4a102a6d0f7f10854
Shaziasama <shaziasamma921@gmail.com>
2024-06-09 16:12:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+//asssigment 1
+//storing the person name in a veriable
+let personnName = "shazia";
+//printing a message to the person
+console.log(`Hello ${"personName"} would you like to learn some python today?`);
---
+++ main.ts
@@ -0,0 +1,7 @@
+//asssigment 1
+//storing the person name in a veriable
+
+let personnName: string= "shazia" ;
+
+//printing a message to the person
+console.log(`Hello ${"personName"} would you like to learn some python today?`);
