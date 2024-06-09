c368ba4780f516d2637b654a1fedd44f3a1b4576
Shaziasama <shaziasamma921@gmail.com>
2024-06-09 19:36:54 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-var itCourse = {
-    courseName: "typescript",
-    location: "governor house",
-    onsiteStudents: 50000,
-};
-console.log(itCourse);
+//making variables
+var five = 5;
+var ten = 10;
+//test 1
+console.log("Test 1: five is equal to 5?");
+console.log(five == 5);
+//tset 2
+console.log("Test 2:ten is equal to 10?");
+console.log(ten == 10);
+//test 3
+console.log("Test 3: five is not equal to 10?");
+console.log(five != 10);
+//test 4
+console.log("Test 4:   ten is greather than 5?");
+console.log(ten > five);
+//test 5
+console.log("Test 5: 5 is smaller than 10?");
+console.log(five < 10);
+//test 6
+console.log("Test 6: 10 is smaller than 5 ?");
+console.log(ten < five);
+//test 7
+console.log("Test 7: 5 is equal to 10?");
+console.log(five == 10);
+//test 8
+console.log("Test 8: 10 is not equal to 10?");
+console.log(ten != 10);
+//test 9
+console.log("Test 9: 5 is greater than 10?");
+console.log(five > ten);
+//test 10
+console.log("Test 10: 10 is smaller than 5?");
+console.log(ten < five);
--- main.ts
+++ main.ts
@@ -1,17 +1,44 @@
-interface itCourse {
-    courseName: string;
-    location: string;
-    onsiteStudents:number;
-}
+//making variables
+let five = 5
 
+let ten =10
 
+//test 1
+console.log("Test 1: five is equal to 5?");
+console.log(five ==5);
 
-let itCourse = {
-    courseName: "typescript",
-    location: "governor house",
-    onsiteStudents: 50000,
+//tset 2
+console.log("Test 2:ten is equal to 10?");
+console.log(ten ==10);
 
+//test 3
+console.log("Test 3: five is not equal to 10?");
+console.log(five !=10);
 
-};
+//test 4
+console.log("Test 4:   ten is greather than 5?");
+console.log(ten > five);
 
-console.log(itCourse);
+//test 5
+console.log("Test 5: 5 is smaller than 10?");
+console.log(five  < 10);
+
+//test 6
+console.log("Test 6: 10 is smaller than 5 ?");
+console.log(ten < five);
+
+//test 7
+console.log("Test 7: 5 is equal to 10?");
+console.log (five ==10);
+
+//test 8
+console.log("Test 8: 10 is not equal to 10?");
+console.log(ten != 10);
+
+//test 9
+console.log("Test 9: 5 is greater than 10?")
+console.log(five > ten);
+
+//test 10
+console.log("Test 10: 10 is smaller than 5?");
+console.log (ten < five);
