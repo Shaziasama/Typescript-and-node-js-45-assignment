ffb98a85009e8a8d32a18abb7c6dbff6af8db508
Shaziasama <shaziasamma921@gmail.com>
2024-06-09 19:56:15 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-// define the function show megicain name
-function show_magicains(magicains) {
-    magicains.forEach(function (name) { return console.log(name); });
+function makeSandwich() {
+    var items = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        items[_i] = arguments[_i];
+    }
+    console.log("Making a sandwich with the following items:");
+    items.forEach(function (singleItem) { return console.log(singleItem); });
+    console.log("Now Enjoy Sandwich!");
 }
-function make_great(magicians) {
-    return magicians.map(function (name) { return "The great ".concat(name); });
-}
-//define an arry of magicians names
-var magician_names = ["Albert", "herry potter", "John"];
-//making of copy original arry through . slice() funnction
-var copy_magicians_names = magician_names.slice();
-//mofify the copy arry include "The great" with theirs names
-var copy_great_magicians = make_great(copy_magicians_names);
-//show both arrays original and copied
-show_magicains(magician_names);
-show_magicains(copy_great_magicians);
+//call the function 3 items with different number of arruguments
+makeSandwich("chickhen", "cheese", "Mayo", "Egg");
+makeSandwich("Bread", "Butter");
+makeSandwich("Bread", "Butter", "Egg", "Cheese", "Mayo");
--- main.ts
+++ main.ts
@@ -1,20 +1,14 @@
-// define the function show megicain name
-function show_magicains(magicains: string[]){
-    magicains.forEach(name => console.log(name)); 
-        }
-function make_great(magicians: string[]){
-    return magicians.map(name => `The great ${name}`);
+
+
+function makeSandwich(...items: string[]){
+console.log("Making a sandwich with the following items:");
+
+items.forEach(singleItem => console.log(singleItem));
+
+console.log("Now Enjoy Sandwich!");
+
 }
-//define an arry of magicians names
-let  magician_names =["Albert", "herry potter","John"]
-
-//making of copy original arry through . slice() funnction
-let copy_magicians_names = magician_names.slice();
-
-//mofify the copy arry include "The great" with theirs names
- let copy_great_magicians = make_great(copy_magicians_names);
-
- //show both arrays original and copied
-
- show_magicains(magician_names);
- show_magicains(copy_great_magicians);
+//call the function 3 items with different number of arruguments
+makeSandwich("chickhen", "cheese", "Mayo","Egg");
+makeSandwich("Bread","Butter");
+makeSandwich("Bread","Butter","Egg","Cheese","Mayo");
