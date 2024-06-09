c3f5abe04017dcb08aa43654ec3b6100a7b7de59
Shaziasama <shaziasamma921@gmail.com>
2024-06-09 19:38:01 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,44 +1,69 @@
-//making variables
-let five = 5
+let apple = "apple";
+let uppercaseApple= "APPLE";
+let ten = 10;
+let twenty = 20;
+let weather = ["Summer","Winter","Autumn" ,"Spring"]
+//test equality and inequality with string
+console.log("is apple is equal to apple?");
+console.log(apple == "apple");
 
-let ten =10
+console.log("is apple is not equal to apple?");
+console.log(apple != "apple");
 
-//test 1
-console.log("Test 1: five is equal to 5?");
-console.log(five ==5);
+//testing using lowercase function
+ console.log("is APPLE is equal to apple convernting to lowercase?"); 
+ console.log(uppercaseApple.toLowerCase() == "apple");
+ console.log("is APPLE is not equal to apple convernting to lowercase?");
+ console.log(uppercaseApple.toLowerCase() != "apple");
 
-//tset 2
-console.log("Test 2:ten is equal to 10?");
-console.log(ten ==10);
+ //numerical test
+ console.log("is ten  is equal to twenty?");
+ console.log(ten == twenty);
+// not equal to
+ console.log("is ten  is not equal to twenty?");
+ console.log(ten != twenty);
 
-//test 3
-console.log("Test 3: five is not equal to 10?");
-console.log(five !=10);
+ //greather than
+ console.log("is ten is greather than twenty?");
+ console.log(ten > twenty);
 
-//test 4
-console.log("Test 4:   ten is greather than 5?");
-console.log(ten > five);
+ //less than
+ console.log("is ten is less than twenty?");
+ console.log(ten < twenty);
 
-//test 5
-console.log("Test 5: 5 is smaller than 10?");
-console.log(five  < 10);
+ //greater than or equal to
+ console.log("ten is grether than or equal to twenty?");
+ console.log(ten >= twenty);
 
-//test 6
-console.log("Test 6: 10 is smaller than 5 ?");
-console.log(ten < five);
+ //less than or equal to
+ console.log(" ten is smaller than or equal to twenty?");
+console.log(ten <= twenty);
 
-//test 7
-console.log("Test 7: 5 is equal to 10?");
-console.log (five ==10);
+//test using "and"& "or"operators
+//using "and"&8
+console.log("twenty is not equal to ten and twenty is greather than ten");
+console.log(twenty !=  10 && twenty > 10);
 
-//test 8
-console.log("Test 8: 10 is not equal to 10?");
-console.log(ten != 10);
+console.log("ten is equal to twenty or ten is greather than twenty");
+console.log(twenty ==10 && ten > twenty)
 
-//test 9
-console.log("Test 9: 5 is greater than 10?")
-console.log(five > ten);
+//using "or" ||
+console.log("ten is  not equal to twenty or twenty is greather than ten");
+ console.log(twenty != 10 || twenty > 10);
 
-//test 10
-console.log("Test 10: 10 is smaller than 5?");
-console.log (ten < five);
+ console.log("twenty is equal to ten or tewnty is smaller than ten");
+ console.log(twenty == 10 || twenty < 10);
+
+ // using array
+ console.log("weather names in arry")
+ console.log(weather);
+
+ //include summer weather in arry
+ console.log("is summer weather included in array?");
+ console.log(weather.includes("Summer"));
+
+ //not include in arry
+ console.log("is summer weather not included in array?");
+ console.log(!weather.includes("Summer"));
+
+ 
