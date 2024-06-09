e3aa6c358c253b2313c07e2ae2d26f71a0d5eeec
Shaziasama <shaziasamma921@gmail.com>
2024-06-09 19:53:11 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-function city_country(city, country) {
-    return "".concat(city, " ,").concat(country);
+function make_album(artist_name, album_title, tracks) {
+    var album = {
+        artist: artist_name,
+        title: album_title,
+    };
+    if (tracks !== undefined) {
+        album.tracks = tracks;
+    }
+    return album;
 }
-//calling a function print the return value
-city_country(" Karachi", "Pakistan");
-console.log(city_country(" Karachi", "Pakistan"));
-console.log(city_country("Tokoyo", "Japan"));
+//calling three function and creating three variables with different values
+var album1 = make_album("shazia", "Album title 1");
+var album2 = make_album("Hashir", "Album title 2");
+var album3 = make_album("sana", "Album title 3 ", 10);
+console.log(album1);
+console.log(album2);
+console.log(album3);
--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
-function city_country(city: string , country: string) : string{
-  return`${city} ,${country}`
-}
-//calling a function print the return value
-city_country(" Karachi" ,"Pakistan");
+function make_album(artist_name: string, album_title: string, tracks?: number){
+ let album:{artist: string, title: string ,tracks?: number} = {
+     artist: artist_name,
+     title: album_title,
+     };
 
-console.log(city_country(" Karachi" , "Pakistan"));
+     if( tracks !== undefined){
+        album.tracks = tracks;
+     }
+     return album;
+ }
+ //calling three function and creating three variables with different values
 
- console.log(city_country("Tokoyo" ,  "Japan"));
+ let album1 = make_album("shazia" ,"Album title 1");
+
+ let album2 = make_album( "Hashir" ,"Album title 2");
+
+ let album3 = make_album( "sana" ,"Album title 3 ", 10);
+
+ console.log(album1);
+ console.log(album2);
+ console.log(album3);
