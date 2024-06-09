635ff723cad32e8d5728524ac3e5bfce09d50388
Shaziasama <shaziasamma921@gmail.com>
2024-06-09 17:05:54 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ let guestLists = ["hina", "arslan", "ubaid"];
 //print the name who cant attend dinner
 let unableAttend = guestLists[0];
 console.log(`${unableAttend} not invited for dinner`);
-let personAttend = ("Ahmed");
-//replacing the name into array
-guestLists[guestLists.indexOf(unableAttend)] = personAttend;
-guestLists.forEach(name => {
-    console.log(`Dear ${name} you are invited to dinner!`);
-});
+//add or remove guest from guest list array
+guestLists.splice(0, 1, "Ahmed");
+//  message print for bigger table
+console.log("Good news! we have found a bigger table for dinner");
+//adding a new guest at strting index of array
+guestLists.unshift("Ali");
+//adding a new guest at ending index of array
+guestLists.push("zain");
+// get a middle index of array
+let middleIndex = Math.floor(guestLists.length / 2);
+//adding a new guest middle index of array
+guestLists.splice(middleIndex, 0, "Usama");
+console.log("updated list of our guest");
+guestLists.forEach(oneguest => console.log(`salam ${oneguest} ,would you like to dinner with me`));
--- main.ts
+++ main.ts
@@ -5,13 +5,27 @@ let guestLists: string[] = ["hina", "arslan","ubaid" ];
 let unableAttend: string = guestLists[0];
 console.log(`${unableAttend} not invited for dinner`);
 
-let personAttend: string = ("Ahmed");
-//replacing the name into array
-guestLists[guestLists.indexOf(unableAttend)] = personAttend;
+//add or remove guest from guest list array
+guestLists.splice(0, 1 ,"Ahmed");
 
-guestLists.forEach(name => {
-    console.log(`Dear ${name} you are invited to dinner!`);
-    
-}); 
+//  message print for bigger table
+console.log("Good news! we have found a bigger table for dinner");
+
+//adding a new guest at strting index of array
+guestLists.unshift("Ali");
+
+//adding a new guest at ending index of array
+guestLists.push("zain");
+
+// get a middle index of array
+let middleIndex: number = Math.floor(guestLists.length / 2);
+
+//adding a new guest middle index of array
+guestLists.splice(middleIndex, 0,"Usama");
+console.log("updated list of our guest");
+
+guestLists.forEach(oneguest =>console.log(`salam ${oneguest} ,would you like to dinner with me`));
+
+
     
 
